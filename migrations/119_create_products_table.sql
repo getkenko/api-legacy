196982a25273c212094e21f243164c5241d81bf4
swaglord <versioncontrol.undamaged050@passinbox.com>
2025-06-27 22:17:48 +0200

    allow for zero macro in products table

--- migrations/119_create_products_table.sql
+++ migrations/119_create_products_table.sql
@@ -3,10 +3,10 @@ CREATE TABLE products (
     name TEXT NOT NULL,
     barcode INT NOT NULL,
     ingredients TEXT NOT NULL,
-    calories INT NOT NULL CHECK (calories > 0),
-    proteins INT NOT NULL CHECK (proteins > 0),
-    fats INT NOT NULL CHECK (fats > 0),
-    carbohydrates INT NOT NULl CHECK (carbohydrates > 0),
+    calories INT NOT NULL CHECK (calories >= 0),
+    proteins INT NOT NULL CHECK (proteins >= 0),
+    fats INT NOT NULL CHECK (fats >= 0),
+    carbohydrates INT NOT NULl CHECK (carbohydrates >= 0),
 
     search_vector tsvector -- so i can work on searching products without having to migrate data yet
 )
