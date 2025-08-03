70f5be931334870993ca12a7db5a549367d9f5f2
idkwhatname <versioncontrol.undamaged050@passinbox.com>
2025-08-03 23:20:00 +0200

    skip serializing nones

--- src/models/dto/products.rs
+++ src/models/dto/products.rs
@@ -15,7 +15,9 @@ pub struct ProductView {
     pub id: Uuid,
     pub name: String,
     pub barcode: i64,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub brand: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub ingredients: Option<String>,
     pub unit: Unit,
     pub quantity: i32,
